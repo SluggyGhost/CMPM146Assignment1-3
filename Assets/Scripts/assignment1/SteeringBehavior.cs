874f68b9b04995f67824530dc7bb8439b2d36503
josette <joey@eduroam-169-233-163-124.ucsc.edu>
2025-04-17 12:37:34 -0700

    first test:

--- Assets/Scripts/assignment1/SteeringBehavior.cs
+++ Assets/Scripts/assignment1/SteeringBehavior.cs
@@ -7,11 +7,14 @@ public class SteeringBehavior : MonoBehaviour
     public Vector3 target;
     public KinematicBehavior kinematic;
     public List<Vector3> path;
-    // you can use this label to show debug information,
-    // like the distance to the (next) target
     public TextMeshProUGUI label;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private int currentPathIndex = 0;
+    private float arriveThreshold = 0.5f;
+    private float maxSpeed = 5f;
+    private float minSpeed = 2f;
+    private float turnSensitivity = 5f;
+
     void Start()
     {
         kinematic = GetComponent<KinematicBehavior>();
@@ -20,30 +23,91 @@ public class SteeringBehavior : MonoBehaviour
         EventBus.OnSetMap += SetMap;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Assignment 1: If a single target was set, move to that target
-        //                If a path was set, follow that path ("tightly")
+        if (path != null && path.Count > 0)
+        {
+            FollowPath();
+        }
+        else
+        {
+            SeekSingleTarget();
+        }
 
-        // you can use kinematic.SetDesiredSpeed(...) and kinematic.SetDesiredRotationalVelocity(...)
-        //    to "request" acceleration/decceleration to a target speed/rotational velocity
+        // Show debug label (distance to next point/target)
+        float distance = (path != null && path.Count > 0)
+            ? Vector3.Distance(transform.position, path[currentPathIndex])
+            : Vector3.Distance(transform.position, target);
+        if (label != null)
+            label.text = $"Dist: {distance:F2}";
+    }
+
+    void SeekSingleTarget()
+    {
+        Vector3 direction = target - transform.position;
+        direction.y = 0; // Ignore vertical difference
+        float distance = direction.magnitude;
+
+        if (distance < arriveThreshold)
+        {
+            kinematic.SetDesiredSpeed(0);
+            return;
+        }
+
+        direction.Normalize();
+        float desiredSpeed = Mathf.Lerp(minSpeed, maxSpeed, distance / 10f); // Smooth acceleration
+        kinematic.SetDesiredSpeed(desiredSpeed);
+        kinematic.SetDesiredRotationalVelocity(Quaternion.LookRotation(direction).eulerAngles.y);
+    }
+
+    void FollowPath()
+    {
+        if (currentPathIndex >= path.Count)
+        {
+            path = null;
+            return;
+        }
+
+        Vector3 waypoint = path[currentPathIndex];
+        Vector3 direction = waypoint - transform.position;
+        direction.y = 0;
+        float distance = direction.magnitude;
+
+        // Go to next waypoint if close enough
+        if (distance < arriveThreshold)
+        {
+            currentPathIndex++;
+            return;
+        }
+
+        direction.Normalize();
+
+        // Adjust speed based on angle to next waypoint
+        float angle = Quaternion.Angle(transform.rotation, Quaternion.LookRotation(direction));
+        float speed = Mathf.Lerp(maxSpeed, minSpeed, angle / 90f); // slower on sharper turns
+
+        kinematic.SetDesiredSpeed(speed);
+        kinematic.SetDesiredRotationalVelocity(Quaternion.LookRotation(direction).eulerAngles.y);
     }
 
     public void SetTarget(Vector3 target)
     {
         this.target = target;
+        this.path = null;
+        currentPathIndex = 0;
         EventBus.ShowTarget(target);
     }
 
     public void SetPath(List<Vector3> path)
     {
         this.path = path;
+        currentPathIndex = 0;
     }
 
     public void SetMap(List<Wall> outline)
     {
         this.path = null;
         this.target = transform.position;
+        currentPathIndex = 0;
     }
 }
